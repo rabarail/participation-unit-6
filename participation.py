755217e7695f5cb32893856a9f85db5227c3a311
Rajan <rajani.kami004@gmailcom>
2026-02-20 18:09:28 -0500

    Defining pets in dictionary

--- participation.py
+++ participation.py
@@ -1,4 +1,31 @@
 """Rajani Baraili, Feb 20 2026,
-Stores pet in dictionary with type of animal and owner and prints list"
+Stores pet in dictionary with type of animal and owner and prints list
+"""
 
-`````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+
+pets = {
+    "whiskers": {
+        "animal": "bunny",
+        "owner": "sara s"
+    },
+
+    "buddy": {
+        "animal": "dog",
+        "owner": "mira m"
+    },
+
+    "coco": {
+        "animal": "cat",
+        "owner": "kira a"
+    },
+    "mimi": {
+        "animal": "dog",
+        "owner": "emma m"
+    },
+
+    "sisca": {
+        "animal": "bunny",
+        "owner": "nina a"
+    }
+
+}
